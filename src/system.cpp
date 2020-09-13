07c92d371a097ed8539722b701a3758cf806fb2d
Bodhisatwa Roy <broy@adobe.com>
2020-09-13 20:34:59 +0530

    2 fixes

--- src/system.cpp
+++ src/system.cpp
@@ -14,37 +14,36 @@ using std::size_t;
 using std::string;
 using std::vector;
 
-System::System(): os_(LinuxParser::OperatingSystem()), kernel_(LinuxParser::Kernel())
-{
-
-        // Initialize processor
-        Processor processor;
-        cpu_ = processor;
-
-        // Initialize processes
-        vector<int> pids = LinuxParser::Pids();
-        for (int pid : pids) 
-	{
-            processes_.push_back(Process(pid));
-        }
-
-};
-
 
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() {return processes_;}
+vector<Process>& System::Processes() 
+{
+        processes_.clear();
+        vector<int> proc = LinuxParser::Pids();
+        for (auto i : proc)
+        {
+                Process temp(i);
+                processes_.push_back(temp);
+        }
+        std::sort(processes_.begin(), processes_.end(),[](Process a, Process b) {
+              return a.CpuUtilization() > b.CpuUtilization();
+            });
+        return processes_;
+
+}
+
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return kernel_; }
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return os_; }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
